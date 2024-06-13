b9ece5f69008f11cdf1814c40dcf1c9ea75b4411
bob <c2064179@newcastle.ac.uk>
2024-06-14 07:51:44 +0800

    Added Graphic Card table to database. Added retailer column in CPU table

--- main/models.py
+++ main/models.py
@@ -17,6 +17,20 @@ class CPU(models.Model):
     PCBC = models.CharField(max_length=255, null=True, blank=True)  # Performance Core Boost CLock
     IG = models.CharField(max_length=255, null=True, blank=True)  # Integrated Graphics
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    retailer = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class GraphicCard(models.Model):
+    name = models.CharField(max_length=255)
+    chipset = models.CharField(max_length=255, null=True, blank=True)
+    memory = models.IntegerField()
+    core_clock = models.IntegerField(null=True, blank=True)
+    boost_clock = models.IntegerField(null=True, blank=True)
+    length = models.IntegerField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    retailer = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
