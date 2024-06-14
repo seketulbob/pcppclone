a8b50423cfb19cea145908ad97b6d56c084bd79e
bob <c2064179@newcastle.ac.uk>
2024-06-14 08:46:06 +0800

    Added display function for Motherboard, Memory, Storage, Case and PowerSupply

--- main/builder.py
+++ main/builder.py
@@ -1,7 +1,22 @@
-from .models import CPU, GraphicCard
+from .models import CPU, GraphicCard, Motherboard, Memory, Storage, Case, PowerSupply
 
 def display_cpu():
     return CPU.objects.all()
 
 def display_graphic_card():
     return GraphicCard.objects.all()
+
+def display_motherboards():
+    return Motherboard.objects.all()
+
+def display_memories():
+    return Memory.objects.all()
+
+def display_storages():
+    return Storage.objects.all()
+
+def display_cases():
+    return Case.objects.all()
+
+def display_power_supplies():
+    return PowerSupply.objects.all()
